34741857b4774f63f29fe118da9fc96a06aec7a8
muhacokotuha <marat.zinatullin@yahoo.com>
2019-06-25 19:51:31 -0400

    Added hamburger menu

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -5,42 +5,46 @@ class Navbar extends React.Component {
   render() {
     return (
       <div className="navbar">
-        <img className="navbar__logo" src="logo.jpg" alt="logo" />
-        <div className="navbar__container__menu navbar__container__menu__website">
-          <ul className="navbar__container__menu__ul">
-            <li className="navbar__container__menu__website__ul__li__dropdown">
-              Shop
-              <div className="navbar__container__menu__website__ul__li__dropdown-content">
-                <a
-                  className="navbar__container__menu__website__ul__li__dropdown-content__a"
-                  href="#"
-                >
-                  Bits
-                </a>
-              </div>
-            </li>
-            <li className="navbar__container__menu__website__ul__li">
-              Delivery
-            </li>
-            <li className="navbar__container__menu__website__ul__li">
-              Contact Us
-            </li>
-          </ul>
+        <div className="navbar__main-menu">
+          <img className="navbar__main-menu__logo" src="logo.jpg" alt="logo" />
+          <div className="navbar__main-menu__container navbar__main-menu__container__website">
+            <ul className="navbar__main-menu__container__ul">
+              <li className="navbar__main-menu__container__website__ul__li__dropdown">
+                Shop
+                <div className="navbar__main-menu__container__website__ul__li__dropdown-content">
+                  <a
+                    className="navbar__main-menu__container__website__ul__li__dropdown-content__a"
+                    href="#"
+                  >
+                    Bits
+                  </a>
+                </div>
+              </li>
+              <li className="navbar__main-menu__container__website__ul__li">
+                Delivery
+              </li>
+              <li className="navbar__main-menu__container__website__ul__li">
+                Contact Us
+              </li>
+            </ul>
+          </div>
+          <div className="navbar__main-menu__container">
+            <ul className="navbar__main-menu__container__ul">
+              <li className="navbar__main-menu__container__ul__li search-icon" />
+              <li className="navbar__main-menu__container__ul__li cart-icon" />
+              <li className="navbar__main-menu__container__ul__li login-icon" />
+            </ul>
+          </div>
         </div>
-        <div className="navbar__container__menu">
-          <ul className="navbar__container__menu__ul">
-            <li className="navbar__container__menu__ul__li search-icon" />
-            <li className="navbar__container__menu__ul__li cart-icon" />
-            <li className="navbar__container__menu__ul__li login-icon" />
-          </ul>
+        <div className="navbar__hamburger-menu">
+          <a
+            className="navbar__hamburger-menu__a"
+            href="javascript:void(0);"
+            onclick="myFunction()"
+          >
+            <i className="fa fa-bars" />
+          </a>
         </div>
-        <a
-          href="javascript:void(0);"
-          className="mobile-menu"
-          onclick="myFunction()"
-        >
-          <i className="fa fa-bars" />
-        </a>
       </div>
     );
   }
