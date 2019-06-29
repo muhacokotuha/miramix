3c478b32e5641db8232e7838d8f119bb2080a989
muhacokotuha <marat.zinatullin@yahoo.com>
2019-06-28 22:14:40 -0400

    Added placeholders for Items

--- frontend/src/components/products/Product.jsx
+++ frontend/src/components/products/Product.jsx
@@ -3,148 +3,25 @@ import "./product.css";
 
 class Product extends React.Component {
   render() {
+    let { items } = this.props;
+    let { category } = this.props;
     return (
       <div>
-        <div className="item">
-          <img
-            className="item__image"
-            src="http://cdn.shopify.com/s/files/1/1186/7574/products/20170214_093017_1200x1200.jpg?v=1527113379"
-            alt="Bits"
-          />
-          <div>
-            <div>Name</div>
-            <div>Price</div>
-            <div>Description:</div>
-            <div>QTY: 1</div>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-        <div className="item">
-          <img
-            className="item__image"
-            src="http://cdn.shopify.com/s/files/1/1186/7574/products/20170214_093017_1200x1200.jpg?v=1527113379"
-            alt="Bits"
-          />
-          <div>
-            <div>Name</div>
-            <div>Price</div>
-            <div>Description:</div>
-            <div>QTY: 1</div>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-        <div className="item">
-          <img
-            className="item__image"
-            src="http://cdn.shopify.com/s/files/1/1186/7574/products/20170214_093017_1200x1200.jpg?v=1527113379"
-            alt="Bits"
-          />
-          <div>
-            <div>Name</div>
-            <div>Price</div>
-            <div>Description:</div>
-            <div>QTY: 1</div>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-        <div className="item">
-          <img
-            className="item__image"
-            src="http://cdn.shopify.com/s/files/1/1186/7574/products/20170214_093017_1200x1200.jpg?v=1527113379"
-            alt="Bits"
-          />
-          <div>
-            <div>Name</div>
-            <div>Price</div>
-            <div>Description:</div>
-            <div>QTY: 1</div>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-        <div className="item">
-          <img
-            className="item__image"
-            src="http://cdn.shopify.com/s/files/1/1186/7574/products/20170214_093017_1200x1200.jpg?v=1527113379"
-            alt="Bits"
-          />
-          <div>
-            <div>Name</div>
-            <div>Price</div>
-            <div>Description:</div>
-            <div>QTY: 1</div>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-        <div className="item">
-          <img
-            className="item__image"
-            src="http://cdn.shopify.com/s/files/1/1186/7574/products/20170214_093017_1200x1200.jpg?v=1527113379"
-            alt="Bits"
-          />
-          <div>
-            <div>Name</div>
-            <div>Price</div>
-            <div>Description:</div>
-            <div>QTY: 1</div>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-        <div className="item">
-          <img
-            className="item__image"
-            src="http://cdn.shopify.com/s/files/1/1186/7574/products/20170214_093017_1200x1200.jpg?v=1527113379"
-            alt="Bits"
-          />
-          <div>
-            <div>Name</div>
-            <div>Price</div>
-            <div>Description:</div>
-            <div>QTY: 1</div>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-        <div className="item">
-          <img
-            className="item__image"
-            src="http://cdn.shopify.com/s/files/1/1186/7574/products/20170214_093017_1200x1200.jpg?v=1527113379"
-            alt="Bits"
-          />
-          <div>
-            <div>Name</div>
-            <div>Price</div>
-            <div>Description:</div>
-            <div>QTY: 1</div>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-        <div className="item">
-          <img
-            className="item__image"
-            src="http://cdn.shopify.com/s/files/1/1186/7574/products/20170214_093017_1200x1200.jpg?v=1527113379"
-            alt="Bits"
-          />
-          <div>
-            <div>Name</div>
-            <div>Price</div>
-            <div>Description:</div>
-            <div>QTY: 1</div>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-        <div className="item">
-          <img
-            className="item__image"
-            src="http://cdn.shopify.com/s/files/1/1186/7574/products/20170214_093017_1200x1200.jpg?v=1527113379"
-            alt="Bits"
-          />
-          <div>
-            <div>Name</div>
-            <div>Price</div>
-            <div>Description:</div>
-            <div>QTY: 1</div>
-            <button>Add to Cart</button>
-          </div>
-        </div>
+        <div className="category-header">{category}</div>
+        {items.map(item => {
+          return (
+            <div className="item">
+              <img className="item__image" src={item.imgSrc} />
+              <div className="item__details">
+                <div>Name: {item.name}</div>
+                <div>Price: ${item.price}</div>
+                <div>Description: {item.description}</div>
+                <div>QTY: 1</div>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          );
+        })}
       </div>
     );
   }
